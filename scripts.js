b953e50856d47b254ad5473d46a3dc8be0fd040a
RayQCodes <rquan3@gmu.edu>
2025-06-22 15:33:40 -0400

    redid to do app again

--- scripts.js
+++ scripts.js
@@ -1,29 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("add-task-btn");
-    const inputs = document.getElementById("todo-input");
-    const todolist = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", ()=> {
+    const addTaskButton = document.getElementById("add-task-btn");
+    const todoInput = document.getElementById("todo-input")
+    const todoList = document.getElementById("todo-list")
 
-    button.addEventListener("click", function () {
-        let newtask = inputs.value.trim();
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(task => {renderTasks(task)});
 
-        if (newtask === "") {
-            window.alert("yo u gotta push a value");
-            return;
+    addTaskButton.addEventListener("click", function(){
+        const taskText = todoInput.value.trim();
+        if(taskText === "") return;
+
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false,
         }
 
-        let li = document.createElement("li");
-        li.textContent = newtask;
-        todolist.appendChild(li);
+        tasks.push(newTask);
+        saveTask();
+        renderTasks(newTask);
+        todoInput.value = ""; // CLEAr input
+             
+    })
 
-        let delbutton = document.createElement("button");
 
-        delbutton.textContent = "delete"; // should set text for delete button
-        li.appendChild(delbutton);
+    function saveTask(){
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+    
+    function renderTasks(task){
+        const li = document.createElement("li");
+        li.setAttribute('data-id', task.id)
+        if(task.completed) li.classList.add('completed');
+        li.innerHTML = `
+        <span>${task.text}</span>
+        <button>delete</button>
+        `;
 
-        delbutton.addEventListener("click", function () {
+        li.addEventListener("click", function(e){
+            if (e.target.button === "BUTTON")return;
+            task.completed = !task.completed;
+            li.classList.toggle("completed")
+            saveTask();
+        })
+
+        li.querySelector('button').addEventListener("click", function(e){
+            e.stopPropagation();
+            tasks = tasks.filter(t => t.id !== task.id);
             li.remove();
-        });
-
-        inputs.value = "";
-    });
-});
+            saveTask();
+        })
+        todoList.appendChild(li)
+    }
+})
